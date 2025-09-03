b1b80b30d77553f6233d8b39a828d162340190b7
ErasylBol <123258145+ErasylBol@users.noreply.github.com>
2025-09-04 02:15:26 +0500

    main.py

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ df = pd.read_csv(file_path, encoding='ISO-8859-1')
 
 df['Order Date'] = pd.to_datetime(df['Order Date'], errors='coerce')
 
-print("🔹 Первые 5 строк таблицы:")
+print(" Первые 5 строк таблицы:")
 print(df.head())
 
-print("\n🔹 Информация о столбцах:")
+print("\n Информация о столбцах:")
 print(df.info())
 
-print("\n🔹 Проверка пропущенных значений:")
+print("\n Проверка пропущенных значений:")
 print(df.isnull().sum())
 
 
 print("\nДата заказа: от", df['Order Date'].min(), "до", df['Order Date'].max())
 
 top_profit = df.groupby('Sub-Category')['Profit'].sum().sort_values(ascending=False ).head(5)
-print("\n💰 Топ-5 прибыльных подкатегорий:\n", top_profit)
+print("\n Топ-5 прибыльных подкатегорий:\n", top_profit)
 
 df['Month'] = df['Order Date'].dt.to_period('M')
 
@@ -34,3 +34,4 @@ plt.xlabel('Месяц')
 plt.ylabel('Сумма продаж')
 plt.tight_layout()
 plt.show()
+
